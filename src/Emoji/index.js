3e21beea5ea3982dc0d461d6aa96f5199bc89d0b
Stanley Ong <Stanley92@hotmail.com>
2019-09-22 13:08:20 +0800

    Add categories selector

--- src/Emoji/index.js
+++ src/Emoji/index.js
@@ -1,12 +1,49 @@
 import './index.css';
 
-import React from 'react';
+import React, {useState} from 'react';
+
+import Categories from '../data/Categories';
+import Checkbox from './Checkbox';
 
 const Emoji = () => {
+    const [keywords, setKeyWords] = useState({
+            'categories': [],
+            'search': []
+        }),
+        handleAddKeywords = (val, type) => {
+            const newKeywords = {...keywords};
+            if (type === 'categories') {
+                if (newKeywords.categories.includes(val)) {
+                    newKeywords.categories = newKeywords.categories.filter((keyword) => keyword !== val);
+                } else {
+                    newKeywords.categories.push(val);
+                }
+            } else if (type === 'search') {
+                if (!newKeywords.search.includes(val)) {
+                    newKeywords.search.unshift(val.trim());
+                }
+            }
+            setKeyWords(newKeywords);
+        };
 
     return (
         <div id="emoji-container">
-            Emoji
+            <div id="categories-wrapper">
+                {
+                    Object.keys(Categories).map((val, index) =>
+                        <label className="label-wrapper" key={index} >
+                            <Checkbox
+                                checked={keywords.categories.includes(val)}
+                                onChange={() => handleAddKeywords(val, 'categories')}
+                            />
+                            <span className="label-text">
+                                {val}
+                            </span>
+                        </label>)
+                }
+            </div>
+            <div id="emoji-wrapper">
+            </div>
         </div>
     );
 };
