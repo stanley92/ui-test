2c90afd307bb57304ad2e73ce386f02bcdaa760f
Stanley Ong <Stanley92@hotmail.com>
2019-09-23 21:30:06 +0800

    Add images of my resume

--- src/About/index.js
+++ src/About/index.js
@@ -1,12 +1,15 @@
 import './index.css';
 
 import React from 'react';
+import resumePage1 from '../img/resume_page_1.png';
+import resumePage2 from '../img/resume_page_2.png';
 
 const About = () => {
 
     return (
         <div id="about-container">
-            About
+            <img src={resumePage1} />
+            <img src={resumePage2} />
         </div>
     );
 };
